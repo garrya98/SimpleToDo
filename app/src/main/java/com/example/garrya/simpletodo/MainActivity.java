a21ab8596d151d6c5bfabbdee7d919b845dab8f8
Garry Jamel Archbold Humphries <garrya@fb.com>
2017-06-21 19:32:13 -0700

    Allows the user to edit and save their entries.

--- app/src/main/java/com/example/garrya/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/garrya/simpletodo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.garrya.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,12 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // a numeric code to identify the edit activity
+    public static final int EDIT_REQUEST_CODE = 20;
+    //keys used for passing data between activites
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
 
     //declaring stateful objects here; these will be null before onCreate is called
     ArrayList<String> items;
@@ -72,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         setupListViewLister();
 
     }
+
     public void onAddItem(View v) {
         //obtain a reference to the EditText created with the layout
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
@@ -86,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         //display a notification to the user
         Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
     }
+
     private void setupListViewLister() {
         //set the ListView's itemLongClickListerner
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -101,7 +110,45 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MainActivity", "Removed item " + position);
                 return true;
             }
+
         });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+// set the ListView's regular click listener
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // first parameter is the context, second is the class of the activity to launch
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                //put "extras" into the bundle for access in the edit activity
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+                // brings up the edit activity with the expectation of a result
+                startActivityForResult(i, EDIT_REQUEST_CODE);
+            }
+        });
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // EDIT_REQUEST_CODE defined with constants
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            // extract updated item value from result extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            // get the position of the item which was edited
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+            // update the model with the new item text at the edited position
+            items.set(position, updatedItem);
+            // notify the adapter the model changed
+            itemsAdapter.notifyDataSetChanged();
+            // Store the updated items back to disk
+            writeItems();
+            // notify the user the operation completed OK
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
         }
+    }
+}
+
+
+
 
